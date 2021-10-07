45ebd2c2d851b97cff6aeb1031adfc3bad3f3c70
Daniel <itzdanielmichael@gmail.com>
2021-10-06 22:07:52 -0400

    upd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-first-app';
+  // passing props
+  name = 'Daniel';
 }
